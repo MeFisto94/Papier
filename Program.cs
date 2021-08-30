54994e1c239f143fbb30f550ec8290c0603b7abc
MeFisto94 <MeFisto94@users.noreply.github.com>
2021-08-30 03:09:13 +0200

    Refactoring: Move project and solution building out into dedicated classes, however the solution handling for the task "applyPatches" needs to be added/decided on

--- Program.cs
+++ Program.cs
@@ -71,26 +71,44 @@ namespace Papier
                                                         "will pick the Main Module.\nFound modules: " +
                                                         $"{string.Join(", ", assembly.Modules.Select(m => m.ToString()))}");
                             }
-                            
-                            
 
                             // Move into class: Decompiler or something.
                             var decompiler = new Decompiler(new CSharpDecompiler(assemblyPath, 
                                 new UniversalAssemblyResolver(assemblyPath, !o.IgnoreUnresolvedAssemblies, null),
                                 new DecompilerSettings(LanguageVersion.CSharp7)));
-
-                            PatchRepository patchRepo;
+                            
                             if (!apply)
                             {
-                                patchRepo = new PatchRepository(Path.Combine("work", "sln", assemblyNameWithoutExt), 
-                                    Path.Combine("patches", assemblyNameWithoutExt));
-                                decompiler.DecompileTypes(assembly.Modules.SelectMany(x => x.Types), patchRepo.RepositoryPath);
-                                WriteCSProjAndSln(assembly, buildData, Path.Combine("work", "sln"));
+                                var slnPath = Path.Combine("work", "sln", $"{assemblyNameWithoutExt}-Full");
+                                Directory.CreateDirectory(slnPath);
+                                
+                                decompiler.DecompileTypes(assembly.Modules.SelectMany(x => x.Types), slnPath);
+                                var projB = new ProjectBuilder
+                                {
+                                    ProjectFolder = Path.Combine("work", "sln"),
+                                    ProjectName = $"{assemblyNameWithoutExt}-Full",
+                                    DebugBuild = true
+                                };
+
+                                foreach (var refPath in Directory.GetFiles(buildData))
+                                {
+                                    projB.WithReference(Path.GetFullPath(refPath));
+                                }
+                                projB.Build();
+
+                                var solB = new SolutionBuilder
+                                {
+                                    SolutionFolder = Path.Combine("work", "sln"),
+                                    SolutionName = "DecompiledModules",
+                                    Projects = new List<string>{$"{assemblyNameWithoutExt}-Full"}
+                                };
+                                solB.Build();
+                                
                                 Console.WriteLine("Done creating a visual studio solution at work/sln");
                                 return;
                             }
                             
-                            patchRepo = new PatchRepository(Path.Combine("repos", assemblyNameWithoutExt), 
+                            var patchRepo = new PatchRepository(Path.Combine("repos", assemblyNameWithoutExt), 
                                 Path.Combine("patches", assemblyNameWithoutExt));
 
                             patchRepo.CleanRepo();
@@ -118,7 +136,19 @@ namespace Papier
                                 buildData, o, assemblyPath, ar);
                             
                             Console.WriteLine("Writing the final csproj");
-                            WriteCSProjAndSln(assembly, buildData, "repos");
+                            var pb = new ProjectBuilder
+                            {
+                                ProjectFolder = Path.Combine(buildData, "repos"),
+                                ProjectName = "Assembly-CSharp-Patches",
+                                DebugBuild = true
+                            };
+
+                            foreach (var refPath in Directory.GetFiles(buildData))
+                            {
+                                pb.WithReference(Path.GetFullPath(refPath));
+                            }
+                            
+                            pb.Build();
                         });
                     }
                     break;
@@ -280,60 +310,7 @@ namespace Papier
                     new CSharpParseOptions(Microsoft.CodeAnalysis.CSharp.LanguageVersion.CSharp8), $"{Path.GetFileName(stub)}"));
             }
         }
-
-        private static void WriteCSProjAndSln(AssemblyDefinition assembly, string buildData, string projectFolder)
-        {
-            foreach (var module in assembly.Modules)
-            {
-                var moduleName = Path.GetFileNameWithoutExtension(module.FileName) ?? module.FileName;
-                Console.WriteLine($"Building {moduleName}.csproj");
-                var root = ProjectRootElement.Create();
-                root.DefaultTargets = "Build";
-                var group = root.AddPropertyGroup();
-                group.AddProperty("Configuration", "Debug");
-                group.AddProperty("Platform", "x64");
-                /*group.AddProperty("DefineConstants",
-                    "UNITY_2020_3_12;UNITY_2020_3;UNITY_2020;UNITY_5_3_OR_NEWER;UNITY_5_4_OR_NEWER;UNITY_5_5_OR_NEWER;UNITY_5_6_OR_NEWER;UNITY_2017_1_OR_NEWER;UNITY_2017_2_OR_NEWER;UNITY_2017_3_OR_NEWER;UNITY_2017_4_OR_NEWER;UNITY_2018_1_OR_NEWER;UNITY_2018_2_OR_NEWER;UNITY_2018_3_OR_NEWER;UNITY_2018_4_OR_NEWER;UNITY_2019_1_OR_NEWER;UNITY_2019_2_OR_NEWER;UNITY_2019_3_OR_NEWER;UNITY_2019_4_OR_NEWER;UNITY_2020_1_OR_NEWER;UNITY_2020_2_OR_NEWER;UNITY_2020_3_OR_NEWER;PLATFORM_ARCH_64;UNITY_64;UNITY_INCLUDE_TESTS;USE_SEARCH_ENGINE_API;SCENE_TEMPLATE_MODULE;ENABLE_AR;ENABLE_AUDIO;ENABLE_CACHING;ENABLE_CLOTH;ENABLE_EVENT_QUEUE;ENABLE_MICROPHONE;ENABLE_MULTIPLE_DISPLAYS;ENABLE_PHYSICS;ENABLE_TEXTURE_STREAMING;ENABLE_VIRTUALTEXTURING;ENABLE_UNET;ENABLE_LZMA;ENABLE_UNITYEVENTS;ENABLE_VR;ENABLE_WEBCAM;ENABLE_UNITYWEBREQUEST;ENABLE_WWW;ENABLE_CLOUD_SERVICES;ENABLE_CLOUD_SERVICES_COLLAB;ENABLE_CLOUD_SERVICES_COLLAB_SOFTLOCKS;ENABLE_CLOUD_SERVICES_ADS;ENABLE_CLOUD_SERVICES_USE_WEBREQUEST;ENABLE_CLOUD_SERVICES_CRASH_REPORTING;ENABLE_CLOUD_SERVICES_PURCHASING;ENABLE_CLOUD_SERVICES_ANALYTICS;ENABLE_CLOUD_SERVICES_UNET;ENABLE_CLOUD_SERVICES_BUILD;ENABLE_CLOUD_LICENSE;ENABLE_EDITOR_HUB_LICENSE;ENABLE_WEBSOCKET_CLIENT;ENABLE_DIRECTOR_AUDIO;ENABLE_DIRECTOR_TEXTURE;ENABLE_MANAGED_JOBS;ENABLE_MANAGED_TRANSFORM_JOBS;ENABLE_MANAGED_ANIMATION_JOBS;ENABLE_MANAGED_AUDIO_JOBS;INCLUDE_DYNAMIC_GI;ENABLE_MONO_BDWGC;ENABLE_SCRIPTING_GC_WBARRIERS;PLATFORM_SUPPORTS_MONO;RENDER_SOFTWARE_CURSOR;ENABLE_VIDEO;PLATFORM_STANDALONE;PLATFORM_STANDALONE_WIN;UNITY_STANDALONE_WIN;UNITY_STANDALONE;ENABLE_RUNTIME_GI;ENABLE_MOVIES;ENABLE_NETWORK;ENABLE_CRUNCH_TEXTURE_COMPRESSION;ENABLE_OUT_OF_PROCESS_CRASH_HANDLER;ENABLE_CLUSTER_SYNC;ENABLE_CLUSTERINPUT;PLATFORM_UPDATES_TIME_OUTSIDE_OF_PLAYER_LOOP;GFXDEVICE_WAITFOREVENT_MESSAGEPUMP;ENABLE_WEBSOCKET_HOST;ENABLE_MONO;NET_4_6;ENABLE_PROFILER;DEBUG;TRACE;UNITY_ASSERTIONS;UNITY_EDITOR;UNITY_EDITOR_64;UNITY_EDITOR_WIN;ENABLE_UNITY_COLLECTIONS_CHECKS;ENABLE_BURST_AOT;UNITY_TEAM_LICENSE;ENABLE_CUSTOM_RENDER_TEXTURE;ENABLE_DIRECTOR;ENABLE_LOCALIZATION;ENABLE_SPRITES;ENABLE_TERRAIN;ENABLE_TILEMAP;ENABLE_TIMELINE;ENABLE_LEGACY_INPUT_MANAGER;CSHARP_7_OR_LATER;CSHARP_7_3_OR_NEWER");*/
-                group.AddProperty("OutputType", "Library");
-                group.AddProperty("OutputPath", "bin\\");
-                group.AddProperty("AssemblyName", moduleName);
-                group.AddProperty("TargetFrameworkVersion", "v4.7.1"); // ??
-                group.AddProperty("LangVersion", "8"); // ??
-                
-                var compileSet = root.AddItemGroup();
-                /*module.Types.Where(type => !type.Name.StartsWith("<") || !type.Name.EndsWith(">"))
-                    .Select(x => $"{x.Name}.cs")
-                    .ToList().ForEach(x => compileSet.AddItem("Compile", x));*/
-                compileSet.AddItem("Compile", "*.cs");
-
-                var referenceItemGroup = root.AddItemGroup();
-                foreach (var refPath in Directory.GetFiles(buildData))
-                {
-                    referenceItemGroup.AddItem("Reference", Path.GetFileNameWithoutExtension(refPath))
-                        .AddMetadata("HintPath", Path.GetFullPath(refPath));
-                }
-
-                /*var target = root.AddTarget("Build");
-                var task = target.AddTask("Csc");
-                task.SetParameter("Sources", "@(Compile)");
-                task.SetParameter("OutputAssembly", module.Name);*/
-                root.AddImport(@"$(MSBuildToolsPath)\Microsoft.CSharp.targets");
-                root.Save(Path.Combine(projectFolder, moduleName, $"{moduleName}.csproj"));
-            }
-
-            // TODO: Solution building doesn't work properly.
-            /*Console.WriteLine("Building Raft.sln");
-            SolutionCreator.WriteSolutionFile(Path.GetFullPath(Path.Combine(upstreamPath, "Raft.sln")),
-                assembly.Modules.Select(x =>
-                {
-                    var moduleName = Path.GetFileNameWithoutExtension(x.FileName) ?? x.FileName;
-                    var projName = Path.Combine(upstreamPath, moduleName, $"{x.Name}.csproj");
-                    return new ProjectItem(Path.GetFullPath(projName),
-                        "x64", Guid.NewGuid(),
-                        ProjectTypeGuids.CSharpCore);
-                }));*/
-        }
-
+        
         public static void DeleteDirectory(string targetDir)
         {
             File.SetAttributes(targetDir, FileAttributes.Normal);
---
+++ SolutionBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ICSharpCode.Decompiler.Solution;
+
+namespace Papier
+{
+    public class SolutionBuilder
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        private bool _built = false;
+        public string SolutionFolder;
+        public string SolutionName;
+        public List<string> Projects;
+        
+        public void Build()
+        {
+            if (_built)
+            {
+                throw new InvalidOperationException("Has already been built");
+            }
+            
+            Logger.Info($"Building {SolutionName}.sln with {Projects.Count} project(s)");
+            var pItems = new List<ProjectItem>();
+            foreach (var x in Projects)
+            {
+                var guid = Guid.NewGuid();
+                Logger.Info($"Adding project {x} with random GUID {guid}");
+                pItems.Add(new ProjectItem(Path.GetFullPath(Path.Combine(SolutionFolder, x, $"{x}.csproj")),
+                    "x64", guid, ProjectTypeGuids.CSharpCore));
+            }
+
+            SolutionCreator.WriteSolutionFile(Path.GetFullPath(Path.Combine(SolutionFolder, $"{SolutionName}.sln")), pItems);
+        }
+    }
+}
